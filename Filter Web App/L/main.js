2ef4a6f27608bfe608f352000484765fc95beb66
Pranav Satish Kumar <98470657+TheMegaCoderPro@users.noreply.github.com>
2022-09-07 19:47:49 +0530

    Add files via upload

---
+++ Filter Web App/L/main.js
@@ -0,0 +1,33 @@
+noseX=0;
+noseY=0;
+function preload(){
+    clown_nose = load_image('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+function setup(){
+    canvas = create.canvas(300,300);
+    canvas.center();
+    video = create.Capture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+function modelLoaded(){
+    console.log('PoseNet is Intialized');
+    poseNet.on(poses, gotPoses);
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX;
+        console.log("nose y = " + noseY;
+    }
+}
